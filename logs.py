6107ed3772f81b789abf2b0f6e81801036a37b63
daridev <darideveloper@gmail.com>
2023-09-04 13:14:40 -0600

    feat (logs): log level from .env

--- logs.py
+++ logs.py
@@ -1,9 +1,16 @@
 import os
 import logging
+from dotenv import load_dotenv
+load_dotenv ()
+
+DEBUG = os.getenv ("DEBUG") == "true"
 
 # logs to file
 logging.basicConfig(filename='.log', format='%(asctime)s - %(filename)s (%(lineno)s) - %(levelname)s - %(message)s')
 
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+if DEBUG:
+    logger.setLevel(logging.DEBUG)
+else:
+    logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler())
